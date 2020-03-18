64840243ad4cd8f2973827072e9aee91ebf6823e
Six Jonathan <j.six@student.isartdigital.com>
2020-03-18 19:03:33 +0100

    Add torch, spot and obstacle

---
+++ Assets/Scenes/Prototype/Assets/torch/scripts/Torchelight.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+
+public class Torchelight : MonoBehaviour {
+	
+	[SerializeField] public Light TorchLight;
+	public GameObject MainFlame;
+	public GameObject BaseFlame;
+	public GameObject Etincelles;
+	public GameObject Fumee;
+	public float MaxLightIntensity;
+	public float IntensityLight;
+
+	void Start ()
+    {
+		TorchLight.intensity=IntensityLight;
+		MainFlame.GetComponent<ParticleSystem>().emissionRate=IntensityLight*20f;
+		BaseFlame.GetComponent<ParticleSystem>().emissionRate=IntensityLight*15f;	
+		Etincelles.GetComponent<ParticleSystem>().emissionRate=IntensityLight*7f;
+		Fumee.GetComponent<ParticleSystem>().emissionRate=IntensityLight*12f;
+	}
+	
+
+	void Update ()
+    {
+		if (IntensityLight<0) IntensityLight=0;
+		if (IntensityLight>MaxLightIntensity) IntensityLight=MaxLightIntensity;		
+
+		TorchLight.intensity=IntensityLight/2f+Mathf.Lerp(IntensityLight-0.1f,IntensityLight+0.1f,Mathf.Cos(Time.time*30));
+
+		TorchLight.color=new Color(Mathf.Min(IntensityLight/1.5f,1f),Mathf.Min(IntensityLight/2f,1f),0f);
+		MainFlame.GetComponent<ParticleSystem>().emissionRate=IntensityLight*20f;
+		BaseFlame.GetComponent<ParticleSystem>().emissionRate=IntensityLight*15f;
+		Etincelles.GetComponent<ParticleSystem>().emissionRate=IntensityLight*7f;
+		Fumee.GetComponent<ParticleSystem>().emissionRate=IntensityLight*12f;		
+
+	}
+}
