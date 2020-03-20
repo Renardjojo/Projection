56c85021628e8e8a2b9ea359a9b84e0dbe08d11e
Six Jonathan <j.six@student.isartdigital.com>
2020-03-20 10:02:55 +0100

    Add triggerable

--- Assets/Scripts/Interaction/Triggered.cs
+++ Assets/Scripts/Interaction/Triggered.cs
@@ -1,11 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEditor;
+using System;
 
-public interface Triggered
+class Triggered : MonoBehaviour
 {
-    Trigger TriggerList { get; set; }
+    [SerializeField] protected List<Trigger>    TriggerList { get; set; }
+    [SerializeField] protected UnityEvent       OnActivatedEvent;
+    [SerializeField] protected UnityEvent       OnDisabledEvent;
+    [SerializeField] protected bool             isActivate = false;
 
-    void OnActivated();
-    void OnDisabled();
+    public void TryToActivate()
+    {
+        foreach (Trigger trig in TriggerList)
+        {
+            if (!trig.IsOn)
+                return;
+        }
+
+        OnActivated();
+    }
+
+    public void TryToDesactivate()
+    {
+        foreach (Trigger trig in TriggerList)
+        {
+            if (!trig.IsOn)
+            {
+                OnDisabled();
+                return;
+            }
+        }
+    }
+
+    public void OnActivated()
+    {
+        OnActivatedEvent?.Invoke();
+    }
+
+    public void OnDisabled()
+    {
+        OnDisabledEvent?.Invoke();
+    }
 }
