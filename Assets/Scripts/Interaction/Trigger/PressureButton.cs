04fb8dd99e6f6d7261654e320446af86c53a7a80
Thomas DALLARD <t.dallard@debian>
2020-03-20 10:32:33 +0100

    Pressure Button moved into corresponding file

---
+++ Assets/Scripts/Interaction/Trigger/PressureButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+public class PressureButton : MonoBehaviour, Trigger
+{
+    [SerializeField] private uint necessaryPressure = 1;
+    private uint currentPressure = 0;
+
+    public bool IsOn { get; set; }
+
+    public event Action onTriggered;
+    public event Action onUntriggered;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        currentPressure++;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        currentPressure--;
+    }
+
+    private void Update()
+    {
+        if (IsOn && currentPressure < necessaryPressure)
+        {
+            onUntriggered();
+            IsOn = false;
+        }
+        else if (!IsOn && currentPressure >= necessaryPressure)
+        {
+            onTriggered();
+            IsOn = true;
+        }
+    }
+}
