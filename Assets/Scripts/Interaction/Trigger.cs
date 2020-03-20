d9d975db98a62e8068a93fb20e3f08288fe40fe1
Thomas DALLARD <t.dallard@debian>
2020-03-20 10:10:12 +0100

    Trigger and triggered classes added

---
+++ Assets/Scripts/Interaction/Trigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using System;
+
+public interface Trigger
+{
+    public bool IsOn { get; set; }
+
+    public event Action onTriggered;
+    public event Action onUntriggered;
+}
---
+++ Assets/Scripts/Interaction/Triggered.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Triggered
+{
+    public Trigger TriggerList { get; set; }
+
+    public void OnActivated();
+    public void OnDisabled();
+}
