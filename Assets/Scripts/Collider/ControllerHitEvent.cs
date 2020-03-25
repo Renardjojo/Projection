2c4a565bb702a2684488538f99c12bef7abee1ff
Six Jonathan <j.six@student.isartdigital.com>
2020-03-25 18:45:00 +0100

    Fixe bug when player hit the clone. Add TagSelector attribut Add 2 script to manag the collision with rigid or with controller

---
+++ Assets/Scripts/Collider/ControllerHitEvent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEditor;
+
+
+[System.Serializable]
+public struct HitEventInfo
+{
+    [TagSelector]
+    public string collisionWithTag;
+    public UnityEvent OnControllerHit;
+
+}
+
+[RequireComponent(typeof(CharacterController))]
+public class ControllerHitEvent : MonoBehaviour
+{
+    [SerializeField] private HitEventInfo[] listCollisionEventWithSpecificTag; 
+
+    // Update is called once per frame
+    void Update()
+    {}
+
+    void OnControllerColliderHit(ControllerColliderHit collision)
+    {
+        for (int i = 0; i < listCollisionEventWithSpecificTag.Length; i++)
+        {
+            if (collision.gameObject.tag == listCollisionEventWithSpecificTag[i].collisionWithTag)
+            {
+                listCollisionEventWithSpecificTag[i].OnControllerHit?.Invoke();
+            }
+        }
+    }
+}
