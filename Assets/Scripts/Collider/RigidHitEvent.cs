0e3991eaf5b32089b33bc208ebd8560d80dd5d68
Six Jonathan <j.six@student.isartdigital.com>
2020-04-01 17:50:57 +0200

    Fix bug : Player was nether kill when he collied with the clone. Fix solution : Add new script (trigger hit event)

---
+++ Assets/Scripts/Collider/RigidHitEvent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEditor;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RigidHitEvent : MonoBehaviour
+{
+    [SerializeField] private HitEventInfo[] listCollisionEventWithSpecificTag; 
+
+    // Update is called once per frame
+    void Update()
+    {}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        for (int i = 0; i < listCollisionEventWithSpecificTag.Length; i++)
+        {
+            if (collision.gameObject.tag == listCollisionEventWithSpecificTag[i].collisionWithTag)
+            {
+                listCollisionEventWithSpecificTag[i].OnHit?.Invoke();
+            }
+        }
+    }
+}
