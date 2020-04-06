81b68ce8598791a0db31554b9e45c0655eb4a902
Thomas DALLARD <t.dallard@debian>
2020-04-06 13:26:16 +0200

    screenlight became killzonee

---
+++ Assets/Scripts/Movements/PlayerShadow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class PlayerShadow : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerController pc = null;
+
+    private void Awake()
+    {
+        GameDebug.AssertInTransform(pc != null, transform, "pc should not be null");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+                
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("Collision");
+        if (collider.gameObject.layer == LayerMask.NameToLayer("ScreenLight"))
+        {
+            Debug.Log("Collision with  light");
+            pc.ResetShadow();    
+        }
+    }
+}
