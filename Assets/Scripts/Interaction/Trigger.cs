a8182f5c6dde6bab544814478a154743fcec0977
s.amara <s.amara@student.isartdigital.com>
2020-04-08 09:59:02 +0200

    Added the option to give a delay to any action linked to a trigger, on switching on and/or off

--- Assets/Scripts/Interaction/Trigger.cs
+++ Assets/Scripts/Interaction/Trigger.cs
@@ -3,29 +3,68 @@ using System;
 
 public abstract class Trigger : MonoBehaviour
 {
-    [SerializeField]
-    public bool isOn { get; set; }
+    /* ==== User accessible members ==== */
+    [Tooltip("Initial state")]
+    [SerializeField] private bool isOn;
 
+    [Header("Delays")]
+    [Tooltip("Time after which this will react, once this is set \"on\"(seconds)")]
+    [Range(0f, 10f), SerializeField]
+    protected float activationDelay = 0f;
+
+    [Tooltip("Time after which this will react, once this is set \"off\"(seconds)")]
+    [Range(0f, 10f), SerializeField]
+    protected float deactivationDelay = 0f;
+
+    public bool IsOn
+    {
+        get
+        { return isOn; }
+
+        set
+        { isOn = value; applyDelay = true; }
+    }
+
+
+    /* ==== Protected data members ==== */
+
+    protected bool applyDelay = false;
+    protected float timeElapsed = 0f;
+
+
+    /* ==== Actions ==== */
     public event Action OnTriggered   = null;
     public event Action OnUntriggered = null;
 
-    internal void Enable()
+
+    /* ==== Methods ==== */
+    private void Update()
     {
-        isOn = true;
-        OnTriggered?.Invoke();
+        if (applyDelay)
+        {
+            timeElapsed += Time.deltaTime * Time.timeScale;
+
+            if (isOn && timeElapsed >= activationDelay)
+            {
+                OnTriggered?.Invoke();
+                timeElapsed = 0f;
+                applyDelay = false;
+            }
+
+            else if (timeElapsed >= deactivationDelay)
+            {
+                OnUntriggered?.Invoke();
+                timeElapsed = 0f;
+                applyDelay = false;
+            }
+        }
     }
 
-    internal void Disable()
-    {
-        isOn = false;
-        OnUntriggered?.Invoke();
-    }
 
-    internal void Toggle()
+    public void Toggle()
     {
         isOn = !isOn;
-        if (isOn)   OnTriggered?.Invoke();
-        else        OnUntriggered?.Invoke();
+        applyDelay = true;
     }
 }
 
