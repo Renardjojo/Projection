b73ebceca3b8c2582cfb744e26e283162762cf5e
s.amara <s.amara@student.isartdigital.com>
2020-04-10 15:53:55 +0200

    Abstract class that inherits MonoBehaviour, dedicated to play sounds

---
+++ Assets/Scripts/Sound/SoundPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public abstract class SoundPlayer : MonoBehaviour
+{
+    [Header("Sound")]
+
+    [Tooltip("Audio played when the trigger is switched on")]
+    [SerializeField]
+    protected AudioClip switchedOnSound;
+
+    [Tooltip("Audio played when the trigger is switched off")]
+    [SerializeField]
+    protected AudioClip switchedOffSound;
+
+    [Tooltip("Whether to use the \"Switched On\" sound for both state changes")]
+    [SerializeField]
+    protected bool useSameSound;
+
+    protected AudioSource switchedOnAudio;
+    protected AudioSource switchedOffAudio;
+
+    protected abstract void PlaySound();
+}
