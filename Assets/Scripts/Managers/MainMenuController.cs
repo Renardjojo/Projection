39396e1abac7e4f3b005cd96d04046e4c612dc3a
Six Jonathan <j.six@student.isartdigital.com>
2020-04-14 16:23:56 +0200

    When the player start the app for the first time, he going to start the tutorial. After he alway start the HUB

---
+++ Assets/Scripts/Managers/MainMenuController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string newScene;
+
+        if (PlayerPrefs.GetInt("PlayerHaveLoadTutoScene") == 1)
+        {
+            newScene = "HUB";
+        }
+        else
+        {
+            newScene = "Tutorial";
+            PlayerPrefs.SetInt("PlayerHaveLoadTutoScene", 1);
+        }
+
+        if (Application.CanStreamedLevelBeLoaded(newScene))
+        {
+            SceneManager.LoadScene(newScene, LoadSceneMode.Single);
+        }
+        else
+        {
+            Debug.LogError("The Main Scene could not be loaded." +
+                "Make sure " + newScene + " exists."
+                + "also, check file -> build settings to make sure it is valid.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
