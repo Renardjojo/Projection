c1a3226e9c5ce0d2cb85eb142e59dbb5d910a92c
Thomas DALLARD <t.dallard@debian>
2020-04-14 14:55:38 +0200

    menu changed, leaving with escape

--- Assets/Scripts/UI/MainMenu/MenuManager.cs
+++ Assets/Scripts/UI/MainMenu/MenuManager.cs
@@ -9,74 +9,45 @@ using UnityEngine;
 public class MenuManager : MonoBehaviour
 {
     [SerializeField]
-    private UnityEngine.UI.Button[] buttonList = null;
+    private SubMenu[] subMenus = null;
 
-    private uint buttonIndex = 0; 
-
-
-    // Start is called before the first frame update
-    void Start()
+    private bool isSubmenuOpened()
     {
-        
-        //buttonList.OnTriggered += StartGame;
-        //buttonList.OnTriggered += QuitGame;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.W))
+        foreach (SubMenu sub in subMenus)
         {
-            ChangeSelection(System.Math.Max(buttonIndex, 1) - 1); // to prevent doing 0 - 1
+            if (sub.gameObject.activeSelf)
+            {
+                return true;
+            }
         }
+        return false;
+    }
 
-        if (Input.GetKeyDown(KeyCode.S))
+    private void Start()
+    {
+        GameDebug.AssertInTransform(subMenus != null, transform, "subMenus should not be null");
+        GameDebug.AssertInTransform(subMenus.Length != 0, transform, "subMenus should have at least one element");
+        GameDebug.AssertInTransform(subMenus[0] != null, transform, "First element of subMenus should not be null");
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Menu"))
         {
-            ChangeSelection(System.Math.Min(buttonIndex + 1, uint.MaxValue));
+            if (isSubmenuOpened())
+            {
+                foreach (SubMenu sub in subMenus)
+                {
+                    sub.DisableMenu();
+                }
+            }
+            else
+            {
+                if (subMenus.Length != 0)
+                {
+                    subMenus[0].EnableMenu();
+                }
+            }
         }
-
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            RunButton();
-        }
-
-        //GameObject myEventSystem = GameObject.Find("EventSystem");
-        //myEventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(buttonList[buttonIndex].gameObject);
-    }
-
-    void ChangeSelection(uint newIndex)  
-    {
-        if (newIndex >= buttonList.Length)
-            newIndex = (uint)buttonList.Length - 1u;
-        //Mathf.Clamp(newIndex, 0, buttonList.Length - 1);
-
-        // Checking bounds
-        if (newIndex == buttonIndex)
-            return;
-
-        Debug.Log(buttonIndex + " / " + newIndex + " / " + buttonList.Length);
-        Unhowever(buttonList[buttonIndex]);
-        buttonIndex = newIndex;
-        However(buttonList[buttonIndex]);
-    }
-
-    void However(UnityEngine.UI.Button button)
-    {
-        FadeButtonColor(button, button.colors.normalColor);
-    }
-
-    void Unhowever(UnityEngine.UI.Button button)
-    {
-        FadeButtonColor(button, button.colors.pressedColor);
-    }
-
-    static private void FadeButtonColor(UnityEngine.UI.Button button, Color finalColor)
-    {
-        button.targetGraphic.CrossFadeColor(finalColor, button.colors.fadeDuration, true, true);
-    }
-
-    private void RunButton()
-    {
-        buttonList[buttonIndex].onClick?.Invoke();
     }
 }
---
+++ Assets/Scripts/UI/MainMenu/SubMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubMenu : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEngine.UI.Button button = null;
+
+    public void EnableMenu()
+    {
+        // Reset selection to be sure we select the button and set its color
+        UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(null, null);
+        gameObject.SetActive(true);
+
+        if (button != null)
+        {
+            button.Select();
+            //UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(button.gameObject, null);
+        }
+    }
+
+    public void DisableMenu()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void EnableMenu(bool shouldEnable)
+    {
+        if (shouldEnable)
+            EnableMenu();
+        else
+            DisableMenu();
+    }
+
+    public void Switch()
+    {
+        EnableMenu(!gameObject.activeSelf);
+    }
+}
