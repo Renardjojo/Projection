f209e564f8b7b9f0caa5b08b1a7271bfd53024cd
Thomas DALLARD <t.dallard@debian>
2020-04-22 11:18:33 +0200

    serialized variable added for setting menu volume

--- Assets/Scripts/UI/EventSystemExtra.cs
+++ Assets/Scripts/UI/EventSystemExtra.cs
@@ -8,6 +8,7 @@ public class EventSystemExtra : MonoBehaviour
 {
     [SerializeField] private Color unselectedColor = new Color(0.44f, 0.43f, 0.43f);
     [SerializeField] private Color selectedColor   = Color.white;
+    [SerializeField] private float menuVolume = 0.5f;
 
     [SerializeField] private AudioClip changeSelectionClip   = null;
     private AudioSource                changeSelectionSource = null;
@@ -22,8 +23,8 @@ public class EventSystemExtra : MonoBehaviour
     {
         changeSelectionSource = gameObject.AddComponent<AudioSource>();
         changeSelectionSource.clip = changeSelectionClip;
-        changeSelectionSource.volume = 0.5f;
-        submitSource.volume = 0.5f;
+        changeSelectionSource.volume = menuVolume;
+        submitSource.volume = menuVolume;
 
         submitSource = gameObject.AddComponent<AudioSource>();
         submitSource.clip = submitClip;
