6b1fbf3da66479f6bab63f140f8140d8ee0d21fe
Six Jonathan <j.six@student.isartdigital.com>
2020-04-22 21:32:10 +0200

    Remove script follow zoom camera and level manager. Rework cameraManager. The camera zoom and the distance between player is connected

--- Assets/Scripts/CameraZone/CameraGeneralPlan.cs
+++ Assets/Scripts/CameraZone/CameraGeneralPlan.cs
@@ -7,34 +7,23 @@ using Cinemachine;
 public class CameraGeneralPlan : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera camera;
-    [SerializeField] private ZoomCameraBetweenEntities zoomCameraPlayerScript;
-    private Collider collider;
-    private LevelManager levelManager; 
-
     [SerializeField, Range(1f, 25f)] private float generalPlanScale = 7f;
-    [SerializeField, Range(0f, 1f)] private float lerpSpeed = 0.8f;
-    float exPlanScale;
 
+    private CameraManager cameraManager;
 
     private void Awake()
     {
-        collider = GetComponent<Collider>();
         gameObject.layer = LayerMask.NameToLayer("TransparentFX");
-        levelManager = GameObject.Find("GameManager/Manager/LevelManager").GetComponent<LevelManager>();
-        if (levelManager == null)
-            Debug.LogAssertion("LevelManager not found");
-
+        cameraManager = GameObject.Find("GameManager/Manager/CinemachineManager").GetComponent<CameraManager>();
+        if (cameraManager == null)
+            Debug.LogAssertion("CinemachineManager not found");
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (camera.Follow.gameObject == other.gameObject)
         {
-            exPlanScale = zoomCameraPlayerScript.minOrthoSize;
-            zoomCameraPlayerScript.minOrthoSize = generalPlanScale;
-
-            levelManager.StopCameraZoomCoroutine();
-            levelManager.StartCameraZoomCoroutine(LerpScale(generalPlanScale));
+            cameraManager.PlayerEnterInCameraZone(generalPlanScale);
         }
     }
 
@@ -42,26 +31,8 @@ public class CameraGeneralPlan : MonoBehaviour
     {
         if (camera.Follow.gameObject == other.gameObject)
         {
-            zoomCameraPlayerScript.minOrthoSize = exPlanScale;
-
-            levelManager.StopCameraZoomCoroutine();
-            levelManager.StartCameraZoomCoroutine(LerpScale(exPlanScale));
+            cameraManager.PlayerExitCameraZone();
         }
     }
 
-    public IEnumerator LerpScale(float lerpGaol)
-    {
-        float lerpStep = 0f;
-
-        zoomCameraPlayerScript.SetActivate(false);
-        while (lerpStep < 1f) //Espilone
-        {
-            lerpStep += lerpSpeed * Time.unscaledDeltaTime;
-            camera.m_Lens.OrthographicSize = Mathf.Lerp(camera.m_Lens.OrthographicSize, lerpGaol, lerpStep);
-            yield return null;
-        }
-
-        zoomCameraPlayerScript.SetActivate(true);
-        camera.m_Lens.OrthographicSize = lerpGaol;
-    }
 }
---
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Cinemachine;
+
+[RequireComponent(typeof(CinemachineVirtualCamera))]
+public class CameraManager : MonoBehaviour
+{
+    private CinemachineVirtualCamera camera;
+    [SerializeField] GameObject mainEntity      = null;
+    [SerializeField] GameObject secondEntity    = null;
+    [SerializeField] float      distanceOffsetZoomBetweenEntity = 5f;
+
+    private int         playerOnCameraZoneEnterCount = 0;
+    internal float      cameraZoneGoal = 0f;
+    
+    float neutralCameraSize;
+
+    /*
+    [SerializeField, Range(0f, 100f)] float lightOffIntensity;
+    [SerializeField, Range(0f, 100f)] float lightOnIntensity;
+    [SerializeField] AnimationCurve lightCurveFallOut;
+    [SerializeField, Range(1f, 10f)] float fallOutDelay = 1f;
+    */
+
+    float cameraZoomBetweenEntityGoal;
+
+    private void Awake()
+    {
+        camera = GetComponent<CinemachineVirtualCamera>();
+        neutralCameraSize = camera.m_Lens.OrthographicSize;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraZoomBetweenEntityGoal = neutralCameraSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateCameraZoomBetweenEntity();
+
+        if (playerOnCameraZoneEnterCount == 0)
+        {
+            if (cameraZoomBetweenEntityGoal > cameraZoneGoal)
+            {
+                ScaleOrUnscaleCamera(cameraZoomBetweenEntityGoal);
+            }
+            else
+            {
+                ScaleOrUnscaleCamera(cameraZoneGoal);
+            }
+        }
+        else
+        {
+            if (cameraZoomBetweenEntityGoal  > neutralCameraSize)
+            {
+                ScaleOrUnscaleCamera(cameraZoomBetweenEntityGoal);
+            }
+            else
+            {
+                camera.m_Lens.OrthographicSize = neutralCameraSize;
+            }
+        }
+    }
+
+    public void ScaleOrUnscaleCamera(float cameraScale)
+    {
+        camera.m_Lens.OrthographicSize = Mathf.Lerp(camera.m_Lens.OrthographicSize, cameraScale, Time.unscaledDeltaTime);
+    }
+
+    public void UpdateCameraZoomBetweenEntity()
+    {
+        float mainToSecondary = (secondEntity.transform.position - mainEntity.transform.position).magnitude;
+
+        // The orthographic size is the half height of the camera view.
+        // It is multiplied by 1f / cam.m_Lens.Aspect to keep the player and
+        // the shadow on screen horizontally, while not unzooming too much
+        cameraZoomBetweenEntityGoal = neutralCameraSize + (mainToSecondary - distanceOffsetZoomBetweenEntity) * 1f / camera.m_Lens.Aspect;
+    }
+
+    public void PlayerEnterInCameraZone (float cameraScaleGoal)
+    {
+        playerOnCameraZoneEnterCount++;
+        cameraZoneGoal = cameraScaleGoal;
+    }
+
+    public void PlayerExitCameraZone()
+    {
+        playerOnCameraZoneEnterCount--;
+
+        if(playerOnCameraZoneEnterCount < 0)
+        {
+            playerOnCameraZoneEnterCount = 0;
+        }
+    }
+}
